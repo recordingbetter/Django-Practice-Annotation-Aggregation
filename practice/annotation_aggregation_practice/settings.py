ecca406f4e80648571da63ef71cba014d16809ab
Joe Donghee Cho <recordingbetter@gmail.com>
2017-06-07 21:34:22 +0900

    modify settings.py, mall.models.py

--- practice/annotation_aggregation_practice/settings.py
+++ practice/annotation_aggregation_practice/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from django.conf import settings
+
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,6 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'j-v3g=m6@*sod@i3zm)ko!4!tm#c*(@0k5033ovc%mun2j107='
+
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- practice/mall/models.py
+++ practice/mall/models.py
@@ -1,2 +1,12 @@
+
 from django.db import models
 
+
+class Product(models.Model):
+    name = models.CharField('이름', max_length = 150, unique = True)
+    price = models.IntegerField('가격')
+
+
+class OrderLog(models.Model):
+    product = models.ForeignKey(Product)
+    created = models.DateTimeField()
