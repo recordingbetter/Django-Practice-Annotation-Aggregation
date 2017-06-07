f69a78e04e330523e43992b8617261349f7fc519
Joe Donghee Cho <recordingbetter@gmail.com>
2017-06-07 22:10:40 +0900

    add __str__

--- practice/mall/models.py
+++ practice/mall/models.py
@@ -6,7 +6,13 @@ class Product(models.Model):
     name = models.CharField('이름', max_length = 150, unique = True)
     price = models.IntegerField('가격')
 
+    def __str__(self):
+        return '{} : {}'.format(self.name, self.price)
+
 
 class OrderLog(models.Model):
     product = models.ForeignKey(Product)
     created = models.DateTimeField()
+
+    def __str__(self):
+        return '{} : {}'.format(self.product.name, self.created)
